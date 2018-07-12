d61d6e9fcccf85acfa8dc3f3a65770bc7b2d1106
SOFT ENGINEERING <support@soft.engineering>
2018-07-12 11:58:39 +0700

    PH PG. Изменил номера телефонов.

--- www/xn--b1abozbb3a5a.xn--p1ai/include/site-phone.php
+++ www/xn--b1abozbb3a5a.xn--p1ai/include/site-phone.php
@@ -1,4 +1,6 @@
 <?/*?><a href="tel:+79039311524" rel="nofollow"><span>+7 (903) </span>931-15-24</a><?*/?>
 <?/*?><a href="tel:+73833311524" rel="nofollow"><span>+7 (903) </span>331-15-24</a><?*/?>
 
+<?/*?><a href="tel:+79134761824" rel="nofollow"><span>+7 (913) </span>476-18-24</a><?*/?>
+
 <a href="tel:+73833225740" rel="nofollow"><span>+7 (383) </span>322-57-40</a>
